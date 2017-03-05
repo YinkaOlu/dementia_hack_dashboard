b31c23c93b9eb0150040447b605758aaf27eb2e5
YinkaOlu <yinka.olu1@gmail.com>
2017-03-05 06:15:00 -0500

    Dashboard BarGraph

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -2,11 +2,47 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import FlatButton from "material-ui/FlatButton"
+import underscore from 'underscore'
 
 class Dashboard extends React.Component{
     constructor(props){
         super(props);
         this.state = {data: props.data}
+    }
+    componentDidMount(){
+        const groupByCategory = underscore.groupBy(this.state.data, data =>
+        {
+            return data.category
+        });
+        const categories = Object.keys(groupByCategory)
+        google.charts.load('current', {packages: ['corechart', 'bar']});
+        google.charts.setOnLoadCallback(drawMultSeries);
+
+        function drawMultSeries() {
+            const setup = [
+                ['Category', 'Category']
+            ]
+            underscore.each(categories, category =>
+            {
+                setup.push([category, groupByCategory[category].length])
+            });
+            var data = google.visualization.arrayToDataTable(setup);
+
+            var options = {
+                title: 'Questions Asked by Main Cateogory',
+                chartArea: {width: '50%'},
+                hAxis: {
+                    title: 'Frequency',
+                    minValue: 0
+                },
+                vAxis: {
+                    title: 'Category'
+                }
+            };
+
+            var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        }
     }
     displayData(){
         const self = this;
@@ -16,7 +52,8 @@ class Dashboard extends React.Component{
         return(
             <div>
                 <h1>Test</h1>
-                <FlatButton label="test" onClick={this.displayData.bind(this)}/>
+
+                <div id="chart_div"></div>
             </div>
         )
     }
