ae428a6b055786a361f90326c94d151205a3b51e
YinkaOlu <yinka.olu1@gmail.com>
2017-03-05 11:00:51 -0500

    Dashboard BarGraph + Piegraph + linegraph fix

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -2,21 +2,28 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import FlatButton from "material-ui/FlatButton"
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import {Card} from "material-ui/Card"
 import underscore from 'underscore'
 
 class Dashboard extends React.Component{
     constructor(props){
         super(props);
-        this.state = {data: props.data}
+        this.state = {data: props.data, categories: [], subCategories: []}
     }
     componentDidMount(){
         const groupByCategory = underscore.groupBy(this.state.data, data =>
         {
             return data.category
         });
-        const categories = Object.keys(groupByCategory)
-        google.charts.load('current', {packages: ['corechart', 'bar']});
-        google.charts.setOnLoadCallback(drawMultSeries);
+        const categories = Object.keys(groupByCategory);
+        this.setState({categories: categories}, () =>
+        {
+            const select = document.createElement("selectOne");
+            google.charts.load('current', {packages: ['corechart', 'bar']});
+            google.charts.setOnLoadCallback(drawMultSeries);
+        });
 
         function drawMultSeries() {
             const setup = [
@@ -30,7 +37,6 @@ class Dashboard extends React.Component{
 
             var options = {
                 title: 'Questions Asked by Main Cateogory',
-                chartArea: {width: '50%'},
                 hAxis: {
                     title: 'Frequency',
                     minValue: 0
@@ -40,20 +46,112 @@ class Dashboard extends React.Component{
                 }
             };
 
-            var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+            var chart = new google.visualization.BarChart(document.getElementById('bargraph_div'));
             chart.draw(data, options);
         }
     }
-    displayData(){
+    displayPieChart(e){
+        const selectedCategory = e.target.innerHTML.trim()
         const self = this;
-        console.log(self.state.data)
+        const filtered = underscore.filter(this.state.data, data =>
+        {
+            return data.category == selectedCategory
+        });
+        const groupBySubCategory = underscore.groupBy(filtered, data =>
+        {
+            return data.subCategory
+        });
+        const subCategories = Object.keys(groupBySubCategory)
+        self.setState({subCategories: subCategories}, ()=>{
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
+        });
+        function drawChart() {
+
+            const setup = [
+                ['Subject', 'Frequency']
+            ]
+            underscore.each(subCategories, subcategory =>
+            {
+                setup.push([subcategory, groupBySubCategory[subcategory].length])
+            });
+            var data = google.visualization.arrayToDataTable(setup);
+
+            var options = {
+                title: 'My Daily Activities'
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('piegraph_div'));
+
+            chart.draw(data, options);
+        }
+    }
+    displayLineChart(e){
+
+        const selectedSubCategory = e.target.innerHTML.trim()
+        const self = this;
+        const filteredGroup = underscore.filter(self.state.data, data =>
+        {
+            return data.subCategory == selectedSubCategory
+        });
+        google.charts.load('current', {packages: ['corechart', 'line']});
+        google.charts.setOnLoadCallback(drawBasic);
+        function drawBasic() {
+            let setup = {};
+            underscore.each(filteredGroup, (filtered) =>
+            {
+                console.log(filtered);
+                if(setup[filtered.date]){
+                    setup[filtered.date] += 1;
+                }
+                else{
+                    setup[filtered.date] = 1;
+                }
+            });
+            console.log(setup);
+            setup = underscore.pairs(setup);
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'X');
+            data.addColumn('number', 'Question Frequency');
+            data.addRows(setup);
+
+            var options = {
+                hAxis: {
+                    title: 'Date'
+                },
+                vAxis: {
+                    title: 'Frequency'
+                }
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('linegraph_div'));
+            chart.draw(data, options);
+        }
     }
     render(){
         return(
             <div>
-                <h1>Test</h1>
-
-                <div id="chart_div"></div>
+                <Card>
+                    <div id="bargraph_div"></div>
+                </Card>
+                <SelectField onChange={this.displayPieChart.bind(this)}>
+                    {this.state.categories.map((category) =>
+                    {
+                        return (<MenuItem primaryText={category}/>)
+                    })}
+                </SelectField>
+                <Card>
+                    <div id="piegraph_div"></div>
+                </Card>
+                <SelectField onChange={this.displayLineChart.bind(this)}>
+                    {this.state.subCategories.map((category) =>
+                    {
+                        return (<MenuItem primaryText={category}/>)
+                    })}
+                </SelectField>
+                <Card>
+                    <div id="linegraph_div"></div>
+                </Card>
             </div>
         )
     }
--- src/dashboardRedux/Reducer.js
+++ src/dashboardRedux/Reducer.js
@@ -2,6 +2,155 @@
 
 const sampleQuesitons =[
     {
+        "title": "Dry Dishes",
+        "category": "House Work",
+        "subCategory": "Dishes"
+    },
+    {
+        "title": "Vacuum house",
+        "category": "House Work",
+        "subCategory": "Dishes"
+    },
+    {
+        "title": "Watch Favorite Soap Opera",
+        "category": "Entertainment",
+        "subCategory": "Routine"
+    },
+    {
+        "title": "Drive to Drug Store",
+        "category": "Travel",
+        "subCategory": "Routine"
+    },
+    {
+        "title": "Soften Clothes",
+        "category": "House Work",
+        "subCategory": "Laundry"
+    },
+    {
+        "title": "Take Eyedroppers",
+        "category": "Health",
+        "subCategory": "Preventative"
+    },
+    {
+        "title": "Afternoon Stretch",
+        "category": "Health",
+        "subCategory": "Exercise"
+    },
+    {
+        "title": "Walk to the Pet Store",
+        "category": "Travel",
+        "subCategory": "Errands"
+    },
+    {
+        "title": "Clean DishWasher",
+        "category": "House Work",
+        "subCategory": "Dishes"
+    },
+    {
+        "title": "Watch Youtube",
+        "category": "Entertainment",
+        "subCategory": "TV"
+    },
+    {
+        "title": "Drive to Dentist office",
+        "category": "Travel",
+        "subCategory": "Errands"
+    },
+    {
+        "title": "Dry Clothes",
+        "category": "House Work",
+        "subCategory": "Laundry"
+    },
+    {
+        "title": "Use Skin Ointment",
+        "category": "Health",
+        "subCategory": "Preventative"
+    },
+    {
+        "title": "Evening Yoga",
+        "category": "Health",
+        "subCategory": "Exercise"
+    },
+    {
+        "title": "Walk the barber",
+        "category": "Travel",
+        "subCategory": "Routine"
+    },
+
+    {
+        "title": "Boil Rice",
+        "category": "Food",
+        "subCategory": "Preparation"
+    },
+    {
+        "title": "Make Cereal",
+        "category": "Food",
+        "subCategory": "Cooking"
+    },
+    {
+        "title": "Set A/C",
+        "category": "Home work",
+        "subCategory": "Appliance"
+    },
+    {
+        "title": "Got to Therapist",
+        "category": "Travel",
+        "subCategory": "Errands"
+    },
+    {
+        "title": "Mouthwash",
+        "category": "Hygiene",
+        "subCategory": "Oral"
+    },
+    {
+        "title": "Change Dentures",
+        "category": "Hygiene",
+        "subCategory": "Oral"
+    },
+    {
+        "title": "Take Bath",
+        "category": "Hygiene",
+        "subCategory": "Body"
+    },
+    {
+        "title": "File Nailes",
+        "category": "Hygiene",
+        "subCategory": "Body"
+    },
+    {
+        "title": "Make Roti",
+        "category": "Food",
+        "subCategory": "Cooking"
+    },
+    {
+        "title": "Drive to Brother's house",
+        "category": "Travel",
+        "subCategory": "Family"
+    },
+    {
+        "title": "Iron Shirts",
+        "category": "House Work",
+        "subCategory": "Laundry"
+    },
+    {
+        "title": "Take sleeping pills",
+        "category": "Health",
+        "subCategory": "Medication"
+    },
+    {
+        "title": "Morning Walk with Dog",
+        "category": "Leisure",
+        "subCategory": "Excercise"
+    },
+    {
+        "title": "Make Hot Chocolate",
+        "category": "Food",
+        "subCategory": "Drinks"
+    },
+
+
+
+    {
         "title": "Washing Dishes",
         "category": "House Work",
         "subCategory": "Dishes"
@@ -14,7 +163,7 @@ const sampleQuesitons =[
     {
         "title": "Drive to Grocery Store",
         "category": "Travel",
-        "subCategory": "Errand Run"
+        "subCategory": "Errand"
     },
     {
         "title": "Dry Clothes",
@@ -27,30 +176,144 @@ const sampleQuesitons =[
         "subCategory": "Dishes"
     },
     {
-        "title": "Morning Excersise",
+        "title": "Morning Exercise",
         "category": "Health",
-        "subCategory": "Excercise"
+        "subCategory": "Exercise"
     },
     {
         "title": "Walk to the corner store",
         "category": "Travel",
-        "subCategory": "Errand Run"
-    }
+        "subCategory": "Errand"
+    },
+    {
+        "title": "Use Dishwasher",
+        "category": "House Work",
+        "subCategory": "Dishes"
+    },
+    {
+        "title": "Watch Netflix",
+        "category": "Entertainment",
+        "subCategory": "Internet"
+    },
+    {
+        "title": "Drive to Doctor's office",
+        "category": "Travel",
+        "subCategory": "Errand"
+    },
+    {
+        "title": "Fold Clothes",
+        "category": "House Work",
+        "subCategory": "Laundry"
+    },
+    {
+        "title": "Use Skin Ointment",
+        "category": "Health",
+        "subCategory": "Skincare"
+    },
+    {
+        "title": "Evening Yoga",
+        "category": "Health",
+        "subCategory": "Exercise"
+    },
+    {
+        "title": "Walk through park",
+        "category": "Travel",
+        "subCategory": "Errand"
+    },
+
+    {
+        "title": "Fry Eggs",
+        "category": "Food",
+        "subCategory": "Cooking"
+    },
+    {
+        "title": "Make Toast",
+        "category": "Food",
+        "subCategory": "Cooking"
+    },
+    {
+        "title": "Set Heater",
+        "category": "Home work",
+        "subCategory": "Comfort"
+    },
+    {
+        "title": "Got to Optometrist",
+        "category": "Travel",
+        "subCategory": "Errands"
+    },
+    {
+        "title": "Brush Teeth",
+        "category": "Hygiene",
+        "subCategory": "Oral"
+    },
+    {
+        "title": "Floss teeth",
+        "category": "Hygiene",
+        "subCategory": "Oral"
+    },
+    {
+        "title": "Take shower",
+        "category": "Hygiene",
+        "subCategory": "Body"
+    },
+    {
+        "title": "Clip nails",
+        "category": "Hygiene",
+        "subCategory": "Body"
+    },
+    {
+        "title": "Make Spaghetti",
+        "category": "Food",
+        "subCategory": "Cooking"
+    },
+    {
+        "title": "Drive to Sister's house",
+        "category": "Travel",
+        "subCategory": "Family"
+    },
+    {
+        "title": "Starch pants",
+        "category": "House Work",
+        "subCategory": "Laundry"
+    },
+    {
+        "title": "Take vitamins",
+        "category": "Health",
+        "subCategory": "Medication"
+    },
+    {
+        "title": "Evening Walk with Dog",
+        "category": "Leisure",
+        "subCategory": "Dog Walking"
+    },
+    {
+        "title": "Make Lemonade",
+        "category": "Food",
+        "subCategory": "Drinks"
+    },
 ]
+
+const initialState = seedResults().concat(seedResults()).concat(seedResults())
 function seedResults() {
     const data = [];
-    const seedSize = 100;
+    const seedSize = 10;
     for(let i = 0; i < seedSize; i++){
-        const randomNum = Math.floor(Math.random()*sampleQuesitons.length)
-        const randomTask = sampleQuesitons[randomNum]
-        randomTask.failed = randomNum % 2 == 0;
-        randomTask.date = new Date().getTime() - Math.floor((7 * (1000 * 60 * 60 * 24)) * (randomNum/seedSize));
+        const randomNum = getRandom();
+        const randomTask = jQuery.extend(true, {}, sampleQuesitons[getRandom()]);
+
+        randomTask.failed = (Math.floor(getRandom()) % 2 == 0);
+        const fullDate = new Date(new Date().getTime() - (Math.floor(90*(Math.floor(Math.random()*sampleQuesitons.length)/sampleQuesitons.length)) * (1000 * 60 * 60 * 24)));
+        const dateStr = `${fullDate.getMonth()}/${fullDate.getUTCDate()}/${fullDate.getUTCFullYear()}`
+        randomTask.date = dateStr;
         data.push(randomTask)
     }
     return data;
+
+    function getRandom() {
+        return Math.floor(Math.random()*sampleQuesitons.length);
+    }
 }
 
-export default function data(state = seedResults()) {
-    console.log(state)
+export default function data(state = initialState) {
     return state
 }
